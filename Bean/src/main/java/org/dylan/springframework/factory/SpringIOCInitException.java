1e997c26f27c4184fe16613d3fb864e331b83941
Dylan.JiaCheng.Huang@outlook.com <Dylan.JiaCheng.Huang@outlook.com>
2019-03-05 13:04:04 +0800

    Add a custom exception which is thrown when IOC init occurs error

---
+++ Bean/src/main/java/org/dylan/springframework/factory/SpringIOCInitException.java
@@ -0,0 +1,11 @@
+package org.dylan.springframework.factory;
+
+/**
+ * @author Jiacheng Huang
+ * @date 2019/03/05 11:42
+ */
+public class SpringIOCInitException extends Exception {
+    public SpringIOCInitException(String message) {
+        super(message);
+    }
+}
